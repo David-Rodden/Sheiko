f0492e58b28e82b3a43e3c9d1aaa4975c45bfab5
David-Rodden <davidrodden273@gmail.com>
2017-10-25 14:41:01 -0700

    Updated sheiko json for first day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, {Component} from 'react';
-import logo from './svg/logo.svg';
 import world from './img/globe.png';
+import sheikoData from './programs/sheiko37.json';
 import './App.css';
 
 /**
  * React test with spinning world animation - reserved for Sheiko program
  */
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         console.log(props);
+    }
+
+    getData(){
+        return fetch('./programs/sheiko37.json');
+    }
+
+    componentDidMount(){
+        this.getData();
     }
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title">Our Planet</h1>
+                    <h1 className="App-title">Sheiko Workout Finder</h1>
+                    <img src={world} className="App-world"/>
                 </header>
                 <body className="App-body">
-                <img src={world} className="App-world"/>
                 </body>
             </div>
         );
