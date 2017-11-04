4881b7435671208f1b1ae001fb34c5127327a810
Monte Roden <rodenmonte@gmail.com>
2017-11-04 01:10:41 -0700

    Displaying JSON fixes

--- src/components/ExerciseTask.js
+++ src/components/ExerciseTask.js
@@ -8,7 +8,7 @@ class ExerciseTask extends Component {
             marginBottom: "1%",
             fontFamily: "Itim, cursive"
         };
-        const effort = this.props.task.effort !== 1 ? Math.round(this.props.task.effort * 100) + "%" : "a comfortable";
+        const effort = this.props.task.effort !== 0 ? Math.round(this.props.task.effort * 100) + "%" : "a comfortable";
         return (<li className="ExerciseTask" style={style}>
             {this.props.task.sets} Sets of {this.props.task.reps} reps at {effort} effort
             &nbsp;&nbsp;&nbsp;<input type="checkbox" data-tip="Done"/>
