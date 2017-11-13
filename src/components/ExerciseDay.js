06c7d1de3a921e20f6f2c921c86666e27299add2
David-Rodden <davidrodden273@gmail.com>
2017-11-13 11:12:12 -0800

    Chart help button added

--- src/components/ExerciseDay.js
+++ src/components/ExerciseDay.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import ExerciseItem from './ExerciseRoutine';
 import ProgressionChart from "./ProgressionChart";
+import surveyHelp from '../img/chart-help.png';
+import surveyHelpText from '../questionnaire/chart-help.json';
 
 export default class ExerciseDay extends Component {
     constructor() {
         super();
+        this.state = {help: false};
         const experience = localStorage["experience"];
         const pageChosen = sessionStorage.getItem("page");
         this.numeric = pageChosen ? sessionStorage["page"] : experience === "No" ? "29" : experience === "Yes, once" ? "30" : "31";
@@ -22,6 +25,11 @@ export default class ExerciseDay extends Component {
             <div className="ExerciseDay">
                 <div style={{textAlign: "center", fontFamily: "Spectral SC", fontSize: 60}}>Sheiko #{this.numeric}</div>
                 <ProgressionChart data={this.sheikoData}/>
+                <div className="Survey-help" style={{marginLeft: "5%"}}>
+                    <img src={surveyHelp} style={{width: "2%", float: "left", borderRadius: "5px", background: "lightgreen"}}
+                         onClick={() => this.setState({help: !this.state.help})}/>
+                    <p style={{marginLeft: "5%", fontFamily: "Itim, cursive", fontSize: 22}}>{this.state.help ? surveyHelpText.text : null}</p>
+                </div>
                 {exerciseRoutines}
             </div>
         );
